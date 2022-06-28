9007b1f9d92c57b5890b527d5bb58714c64ebeec
yuiben <njhben123@gmail.com>
2022-06-28 11:03:40 +0700

    new branch

--- Hello.py
+++ Hello.py
@@ -1,6 +1,17 @@
-print('Hello word')
-x = 1
-if x == 1:
-    print('x = ' + str(x))
-
-    adadasd asdadasdasd
+x = 10
+y = 20
+z = 30
+no_of_teachers = x
+no_of_male_students = y
+no_of_female_students = z
+ 
+if (no_of_teachers == 10 and no_of_female_students == 30 
+    and no_of_male_students == 20 and (x + y) == 30):
+    print('The course is valid')
+ 
+# This could be done instead:
+ 
+if (no_of_teachers == 10 and no_of_female_students == 30 
+    and no_of_male_students == 20 and x + y == 30):
+    print('The course is valid')
+    aaaaa
